c8876ff8111da57cbe320905a663867b10f07202
rbfcdog <rodrigobfcdog@gmail.com>
2025-10-18 16:31:26 -0300

    supabase

--- backend/src/api/services/supabase.service.ts
+++ backend/src/api/services/supabase.service.ts
@@ -3,16 +3,17 @@ import { ClientInfo } from "../../types";
 
 export class supabaseService {
 
-    static async query_client_info() : Promise<ClientInfo>{
+    static async query_client_info(clientId: string) : Promise<ClientInfo>{
 
         const { data, error } = await supabase
-            .from<ClientInfo>('client_info')
-            .select('*')
-            .single();
+        .from("client_info")
+        .select("*")
+        .eq("patient_id", clientId)
+        .single<ClientInfo>();
 
         if (error) {
             throw new Error(`Error fetching client info: ${error.message}`);
         }
         return data;
     }
-    
+}
