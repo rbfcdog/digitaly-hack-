b461cea2e405b67c243dd5a1946e110510cd56e1
rbfcdog <rodrigobfcdog@gmail.com>
2025-10-18 18:45:56 -0300

    a

--- backend/src/api/routes/actions.router.ts
+++ backend/src/api/routes/actions.router.ts
@@ -4,7 +4,7 @@ import { ActionsController } from "../controllers/actions.controller.js";
 const router = Router();
 
 router.post("/foo", ActionsController.foo);
-router.post("/agent", ActionsController.testAgent); // 👈 nova rota para testar o agente
+router.post("/agent", ActionsController.testAgent); 
 router.post("/query-client-info", ActionsController.queryClientInfo);
 
 export default router;
